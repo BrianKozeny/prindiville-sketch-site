b9b6aa60bce537b9312b1c24ab8dec607b4e4541
Brian Kozeny <briankozeny@Brians-MacBook-Pro.local>
2021-06-29 18:46:09 -0500

    success message + files to index

--- django/dashboard/views.py
+++ django/dashboard/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
+from django.contrib import messages 
 from .models import Video
 from .forms import VideoForm
 
 def index(request):
-    return render(request, "dashboard/index.html")
-
-def showvideo(request):
-
     firstvideo= Video.objects.all()
     videofile = []
     for video in firstvideo:
@@ -22,4 +19,25 @@ def showvideo(request):
               'form': form
               }
 
+
+    return render(request, "dashboard/index.html", context)
+
+def showvideo(request):
+
+    firstvideo= Video.objects.all()
+    videofile = []
+    for video in firstvideo:
+        videofile.append(video.file.url)
+
+
+    form= VideoForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Form submission successful')
+
+    context= {'videos': videofile,
+              'form': form
+              }
+
     return render(request, 'dashboard/videos.html', context)
+
