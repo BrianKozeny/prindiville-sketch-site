fecc3d325bb68399c3e555a4b298b467a7200e66
Brian Kozeny <briankozeny@Brians-MacBook-Pro.local>
2021-07-16 16:20:48 -0500

    File size bug fixed

--- django/dashboard/forms.py
+++ django/dashboard/forms.py
@@ -4,4 +4,4 @@ from .models import Video
 class VideoForm(forms.ModelForm):
     class Meta:
         model = Video
-        fields = ('title', 'description', 'size', 'file')
+        fields = ('title', 'description', 'file')
