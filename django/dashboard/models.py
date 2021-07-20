8c9efb62f1fb701359724dce0cd8890446a89a9e
Brian Kozeny <briankozeny@Brians-MacBook-Pro.local>
2021-07-20 00:00:24 -0500

    finished sketches in cards

--- django/dashboard/models.py
+++ django/dashboard/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class Video(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
-    script = models.FileField(upload_to='videos/', null=True, verbose_name="Script")
-    footage = models.FileField(upload_to='videos/', null=True, verbose_name="Footage")
-    file = models.FileField(upload_to='videos/', null=True, verbose_name="Finished Sketch")
+    script = models.FileField(upload_to='videos/', null=True, blank=True, verbose_name="Script")
+    footage = models.FileField(upload_to='videos/', null=True, blank=True, verbose_name="Footage")
+    file = models.FileField(upload_to='videos/', null=True, blank=True,  verbose_name="Finished Sketch")
 
     def __str__(self):
         return self.title
